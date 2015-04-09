3a2620a063d58e408a444ea44398f90412372f72
Liron Yahdav <liron@handl.it>
2015-04-09 14:57:37 -0700

    WatchKit test app

---
+++ dmWatchKitTest WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  dmWatchKitTest WatchKit Extension
+//
+//  Created by Liron Yahdav on 4/9/15.
+//  Copyright (c) 2015 Liron Yahdav. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ dmWatchKitTest WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  dmWatchKitTest WatchKit Extension
+//
+//  Created by Liron Yahdav on 4/9/15.
+//  Copyright (c) 2015 Liron Yahdav. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
